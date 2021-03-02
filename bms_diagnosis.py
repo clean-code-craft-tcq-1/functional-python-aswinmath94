07dd4dea10e713f92e86dc7bc1975758055676ad
aswinmath94 <49982968+aswinmath94@users.noreply.github.com>
2021-03-02 18:38:14 +0530

    Update and rename check_limits.py to bms_diagnosis.py
    Committed changes in main file

---
+++ bms_diagnosis.py
@@ -0,0 +1,15 @@
+from bms_health import bms_health_is_ok
+from input_validator import bms_parameter_name_validation
+from input_validator import bms_parameter_value_validation
+
+if __name__ == '__main__':
+    assert (bms_parameter_name_validation({'bms_temperature': 40, 'soc': 40, 'charging_rate': 0.8}) == 'UNKNOWN PARAMETER')
+    assert (bms_parameter_name_validation({'temperature': 40, 'soc': 40, 'charging_rate': 0.6}) == 'OK')
+
+    assert (bms_parameter_value_validation({'temperature': 'nan', 'soc': 40, 'charging_rate': 0.8}) == 'INVALID PARAMETER VALUE')
+    assert (bms_parameter_value_validation({'temperature': 40, 'soc': '', 'charging_rate': 0.8}) == 'INVALID PARAMETER VALUE')
+    assert (bms_parameter_value_validation({'temperature': 40, 'soc': 40 , 'charging_rate': 'unknown'}) == 'INVALID PARAMETER VALUE')
+    assert (bms_parameter_name_validation({'temperature': 40, 'soc': 40, 'charging_rate': 0.6}) == 'OK')
+
+    assert (len(bms_health_is_ok({'temperature': 40, 'soc': 40, 'charging_rate': 0.6})) == 0)
+    assert (len(bms_health_is_ok({'temperature': 100, 'soc': 40, 'charging_rate': 0.8})) > 0)
